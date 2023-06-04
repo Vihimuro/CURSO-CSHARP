d313b2b2430c6ce997494ccc6e0eae6a3cc995e6
Vihimuro <vihi1208@gmail.com>
2023-06-04 17:01:46 -0300

    #add Dictionary1 Project

---
+++ Dictionary1/Program.cs
@@ -0,0 +1,27 @@
+﻿Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies["user"] = "emily";
+cookies["email"] = "emily@gmail.com";
+cookies["phone"] = "99999999";
+
+Console.WriteLine(cookies["email"]);
+
+cookies.Remove("email");
+
+if (cookies.ContainsKey("email"))
+{
+    Console.WriteLine(cookies["email"]);
+}
+
+else
+{
+    Console.WriteLine("There is no 'email' key");
+}
+
+Console.WriteLine("Size: " + cookies.Count);
+
+Console.WriteLine("ALL COOKIES: ");
+foreach (KeyValuePair<string, string> item in cookies)
+{
+    Console.WriteLine(item.Key + ": " + item.Value);
+}
