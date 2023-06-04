a0ad84145678f619683089b451eda8accda1c7a8
Vihimuro <vihi1208@gmail.com>
2023-06-04 16:55:14 -0300

    #add Set1 Project

---
+++ Set1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Set1.Entities;
+
+HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(" ");
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            set.Add(new LogRecord { Username = name, Instant = instant });
+        }
+        Console.Write("Total users: " + set.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
