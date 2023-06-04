b3f4d5098bbb1ed3b912d9a23af61aa82150d400
Vihimuro <vihi1208@gmail.com>
2023-06-04 18:12:48 -0300

    #add Linq1 Project

---
+++ Linq1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+//Specify the data source
+int[] numbers = new int[] { 2, 3, 4, 5 };
+
+//Define the query expression
+IEnumerable<int> result = numbers
+    .Where(x => x % 2 == 0)
+    .Select(x => x * 10);
+
+//Execute the query
+foreach (int x in result)
+{
+    Console.WriteLine(x);
+}
