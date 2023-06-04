118b377f1ebedd1d6df4d2d4fc3950eb6193b45b
Vihimuro <vihi1208@gmail.com>
2023-06-04 00:01:34 -0300

    #add Files4 Project

---
+++ Files4/Program.cs
@@ -0,0 +1,25 @@
+﻿string path = @"c:\temp\myfolder";
+
+try
+{
+    IEnumerable<string> folders =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("FOLDERS:");
+    foreach (string folder in folders)
+    {
+        Console.WriteLine(folder);
+    }
+
+    IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("FILES:");
+    foreach (string file in files)
+    {
+        Console.WriteLine(file);
+    }
+
+    Directory.CreateDirectory(path + @"\newFolder");
+   
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred: " +e.Message);
+}
