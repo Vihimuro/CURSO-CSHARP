1927a5413bf02578a5583ab241a3f3ba804ed476
Vihimuro <vihi1208@gmail.com>
2023-06-03 22:17:35 -0300

    #add EmployeesPayments Project

---
+++ EmployeesPayments/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesPayments.Entities
+{
+    internal class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee() { }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
