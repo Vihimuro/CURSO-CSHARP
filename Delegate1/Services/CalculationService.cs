45fd9816616284f8a366d93835e80e3b7cbc9167
Vihimuro <vihi1208@gmail.com>
2023-06-04 17:46:26 -0300

    #add Delegate1 Project

---
+++ Delegate1/Services/CalculationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1.Services
+{
+    internal class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine($"Max: {max}");
+        }
+
+        public static void ShowSum(double x, double y) 
+        {
+            double sum = x + y;
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
