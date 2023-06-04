0bd5849ad17aa9a8cddae60b87c77f355408e179
Vihimuro <vihi1208@gmail.com>
2023-06-04 16:12:51 -0300

    #add HashCode_Equals Project

---
+++ HashCode&Equals/Program.cs
@@ -0,0 +1,11 @@
+﻿using HashCode_Equals.Entities;
+
+Client a = new Client { Name = "Emily", Email = "emily@gmail.com" };
+Client b = new Client { Name = "Vinícius", Email = "emily@gmail.com" };
+
+//O objeto Client está setado para comparar o Email apenas
+
+Console.WriteLine(a.Equals(b)); //True => o conteúdo do email é o mesmo
+Console.WriteLine(a == b); //False => a referência de memória é diferente
+Console.WriteLine(a.GetHashCode()); //mesmo email == hashCode igual
+Console.WriteLine(b.GetHashCode()); //mesmo email == hashCode igual
