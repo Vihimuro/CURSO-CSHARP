76dfac156677631b6767a21007aee491c6da29cc
Vihimuro <vihi1208@gmail.com>
2023-06-04 19:10:35 -0300

    #add Linq2 Project

---
+++ Linq2/Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Linq2.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public Product() { }
+
+        public Product(int id, string name, double price, Category category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString()
+                + ": "
+                + Name
+                + " - $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + " - Tier "
+                + Category.Tier.ToString();
+        }
+    }
+}
